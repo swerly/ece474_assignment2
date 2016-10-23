8a7196521a1281aeb9b7e42a525c9f7241994ed5
swerly <seth.werly@gmail.com>
2016-10-23 15:44:36 -0700

    Added struct definitions
    lists.h holds structures, lists.c holds methods to modify lists

---
+++ lists.h
@@ -0,0 +1,19 @@
+//Structure for each variable/node in our list
+typedef struct node{
+    int isSigned;
+    int width;
+    char* name;
+    struct node* next;
+} varNode;
+
+//structure to hold all of our lists to pass easily between functions
+typedef struct{
+    varNode* inputHead;
+    varNode* outputHead;
+    varNode* regHead;
+    varNode* wireHead;
+} listContainer;
+
+void addToList(varNode* list, varNode* varToAdd);
+//other functions as needed
+//searching for name, etc
